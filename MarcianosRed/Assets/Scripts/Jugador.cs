3034b25708be0528624ac71024d5fcbd4eb3e478
borjafdzan <borjafdzan@gmail.com>
2022-03-03 16:07:00 +0100

    El proyectil se spawnea pero no se mueve se comprueba si es el servidor si no lo es se comprueba si es el owner si lo es se hace una llamada a procedimiento remoto en el servidor

--- MarcianosRed/Assets/Scripts/Jugador.cs
+++ MarcianosRed/Assets/Scripts/Jugador.cs
@@ -7,6 +7,8 @@ public class Jugador : NetworkBehaviour
 {
     [SerializeField]
     private float velocidad = 4;
+    [SerializeField]
+    private GameObject proyectil;
     float entrada;
 
     Vector3 siguientePosicion = Vector2.zero;
@@ -25,13 +27,50 @@ public class Jugador : NetworkBehaviour
         {
             this.transform.Translate(Vector3.right * (entrada * Time.deltaTime * velocidad));
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Disparar();
+        }
     }
 
     //La pantalla tiene un alto de 5 unidades el jugador por defecto esta en alto 0
     //El primer jugador solo se movera 3 unidades hacia abajo y el jugador 2 4 
     //para eso utilizamos el ClientNetworkId
-    private void SituarAlJugadorEnPantalla(){
+    private void SituarAlJugadorEnPantalla()
+    {
         Vector3 posicion = this.transform.position;
         this.transform.position = posicion - (Vector3.up * (2 + OwnerClientId));
+    }
+
+
+
+
+    private void Disparar()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            SpawnProyectil();
+        }
+        else
+        {
+            if (IsOwner)
+            {
+                DispararServerRpc();
+            }
+        }
+    }
+
+    [ServerRpc]
+    private void DispararServerRpc()
+    {
+        SpawnProyectil();
+    }
+
+    private void SpawnProyectil()
+    {
+        GameObject objeto = Instantiate(this.proyectil, new Vector3(this.transform.position.x, this.transform.position.y + 1f, this.transform.position.z), Quaternion.identity);
+        NetworkObject objetoRed = objeto.GetComponent<NetworkObject>();
+        objetoRed.Spawn();
     }
 }
