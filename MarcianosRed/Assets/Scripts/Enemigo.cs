3ab7d6fabff73afc94423447bbcaf2166d59b8de
BorjaFdzAn <BorjaFdzAn@gmail.com>
2022-03-10 20:24:17 +0100

    Correccion en la posicion de los marcianos

--- MarcianosRed/Assets/Scripts/Enemigo.cs
+++ MarcianosRed/Assets/Scripts/Enemigo.cs
@@ -6,19 +6,20 @@ public class Enemigo :  MonoBehaviour
 {
     [SerializeField]
     private float velocidad = 2;
+    Vector2 posicionInicial;
     Vector3 siguientePosicion = Vector3.zero;
     float direccion = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+       this.posicionInicial = this.transform.position; 
     }
 
     // Update is called once per frame
     void Update()
     {
          this.transform.Translate(Vector3.right * Time.deltaTime * direccion * velocidad);
-         if (this.transform.position.x > 5 || this.transform.position.x < -5){
+         if (this.transform.position.x > this.posicionInicial.x + 2 || this.transform.position.x < this.posicionInicial.x - 2){
              direccion *= -1;
          }
     }
--- MarcianosRed/Assets/Scripts/SpawnEnemigos.cs
+++ MarcianosRed/Assets/Scripts/SpawnEnemigos.cs
@@ -8,6 +8,7 @@ public class SpawnEnemigos : MonoBehaviour
     [SerializeField]
     GameObject enemigo;
     int contadorJugadores = 0;
+    int posicionInicialMarciano = -4;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,14 +23,18 @@ public class SpawnEnemigos : MonoBehaviour
 
     private void OnConectarseJugador(ulong idJugador){
         contadorJugadores++;
-        if (contadorJugadores == 2){
-            SpawnEnemigo();
+        if (contadorJugadores == 2 && NetworkManager.Singleton.IsServer){
+            for (int i = 0; i < 10; i++)
+            {
+                SpawnEnemigo(new Vector3(posicionInicialMarciano +i, 4, 0));
+            }
+            
         }
     }
 
-    private void SpawnEnemigo(){
+    private void SpawnEnemigo(Vector3 posicion){
         NetworkManager.Singleton.OnClientConnectedCallback += OnConectarseJugador; 
-        GameObject instanciaEnemigo = Instantiate(enemigo, new Vector3(0, 4, 0), Quaternion.identity);
+        GameObject instanciaEnemigo = Instantiate(enemigo, posicion, Quaternion.identity);
         NetworkObject objetoRed = instanciaEnemigo.GetComponent<NetworkObject>();
         objetoRed.Spawn();
     }
